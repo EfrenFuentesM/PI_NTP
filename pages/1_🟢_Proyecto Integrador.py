98b14fd804a178e85a23df011320896577ecd44c
Efrén Fuentes <fuentesefren7@gamil.com>
2024-10-28 21:03:44 +0100

    Commit de prueba

---
+++ pages/1_🟢_Proyecto Integrador.py
@@ -0,0 +1,52 @@
+import streamlit as st 
+import pandas as pd  
+import firebase_admin  
+from firebase_admin import credentials, firestore  
+
+st.set_page_config(layout="wide")
+
+st.subheader("Proyecto Integrador")
+
+# Verificar si ya existe una instancia de la aplicación
+if not firebase_admin._apps:  
+    # Cargar las credenciales de Firebase desde los secretos de Streamlit
+    firebase_credentials = st.secrets["FIREBASE_CREDENTIALS"]  
+    # Convertir las credenciales a un diccionario Python
+    secrets_dict = firebase_credentials.to_dict()  
+    # Crear un objeto de credenciales usando el diccionario 
+    cred = credentials.Certificate(secrets_dict)  
+    # Inicializar la aplicación de Firebase con las credenciales
+    app = firebase_admin.initialize_app(cred)
+
+# Obtener el cliente de Firestore
+db = firestore.client()
+
+
+tad_descripcion, tab_Generador, tab_datos, tab_Análisis_Exploratorio, tab_Filtrado_Básico, tab_Filtro_Final_Dinámico = st.tabs(["Descripción", "Datos", "Análisis Exploratorio", "Filtrado Básico", "Filtro Final Dinámico"])
+
+#----------------------------------------------------------
+#Generador de datos
+#----------------------------------------------------------
+with tad_descripcion:      
+
+    st.markdown('''   
+
+    ### Introducción
+
+    -   ¿Qué es el proyecto?
+    -   ¿Cuál es el objetivo principal?
+    -   ¿Por qué es importante?
+
+    ### Desarrollo
+
+    -   Explicación detallada del proyecto
+    -   Procedimiento utilizado
+    -   Resultados obtenidos
+
+    ### Conclusión
+
+    -   Resumen de los resultados
+    -   Logros alcanzados
+    -   Dificultades encontradas
+    -   Aportes personales
+    ''')
